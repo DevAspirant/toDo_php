69c97efc7d2def2944893de39325be416f8fec08
ammarfalmban <devAspirant@gmail.com>
2020-04-05 00:02:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html dir="rtl">
+<head>
+<title> المساعد الشخصي </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<div class="header">
+<h1> مهماتي </h1>
+<ul class="tasks">
+    <li>
+    <span class="task"> وصف المهمة </span>
+    <a class="done-button" href="#"> تم الانجاز</a>
+    <p class="date"> 6 - 4 - 2020 اخر تاريخ لانجاز المهمة</p>
+    </li>
+</ul>
+<!-- Form -->
+<form class="task-add">
+    <input class="input" type="text" placeholder="ادخل وصف مهمة جديدة">
+    <input class="input" type="text" placeholder="ادخل تاريخ اخر انجاز">
+    <input class="submit" type="submit" value="آضف">
+</form> 
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
